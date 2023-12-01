150b2180a21b4573bacc377bd3c8916f393cdd5b
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2023-12-01 10:52:08 -0500

    Finish the implementation of UserDao to make it work with User objects

--- src/main/java/data_access/persistence/UserDao.java
+++ src/main/java/data_access/persistence/UserDao.java
@@ -1,16 +1,18 @@
 package data_access.persistence;
 
+import entity.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import java.util.Optional;
 
-public class UserEntityDao {
+public class UserDao {
 
     private final EntityManagerFactory entityManagerFactory;
 
-    public UserEntityDao() {
+    public UserDao() {
         this.entityManagerFactory = Persistence.createEntityManagerFactory("OutfitManager");
     }
 
@@ -18,41 +20,57 @@ public class UserEntityDao {
         this.entityManagerFactory.close();
     }
 
-    public void saveUser(UserEntity userEntity) {
+    public void saveUser(User user) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
         entityManager.getTransaction().begin();
-        entityManager.persist(userEntity);
+        entityManager.persist(UserEntity.fromUser(user));
         entityManager.getTransaction().commit();
+
         entityManager.close();
     }
 
-    public Optional<UserEntity> getUserByUsername(String username) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        UserEntity user = entityManager.find(UserEntity.class, username);
-        entityManager.close();
-        return Optional.ofNullable(user);
+    public boolean existsByUsername(String username) {
+        return getByUsername(username).isPresent();
     }
 
-    public void updateUser(UserEntity userEntity) {
+    public Optional<User> getByUsername(String username) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        UserEntity userEntity = entityManager.find(UserEntity.class, username);
+
+        entityManager.close();
+
+        return Optional.ofNullable(UserEntity.toUser(userEntity));
+    }
+
+    public void updateUser(User user) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
         entityManager.getTransaction().begin();
-        entityManager.merge(userEntity);
+        entityManager.merge(UserEntity.fromUser(user));
         entityManager.getTransaction().commit();
+
         entityManager.close();
     }
 
-    public void deleteUser(UserEntity userEntity) {
+    public void deleteUser(User user) {
+        UserEntity userEntity = UserEntity.fromUser(user);
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+
         entityManager.getTransaction().begin();
         entityManager.remove(entityManager.contains(userEntity) ? userEntity : entityManager.merge(userEntity));
         entityManager.getTransaction().commit();
+
         entityManager.close();
     }
 
     public static void main(String[] args) {
-        var userDao = new UserEntityDao();
-        var user = userDao.getUserByUsername("test");
+        var userDao = new UserDao();
+        var user = userDao.getByUsername("test");
 
-        System.out.println(user.get().getUsername());
+        userDao.deleteUser(user.get());
+
+        System.out.println(userDao.getByUsername("test").isPresent());
     }
 }
--- src/main/java/data_access/persistence/UserEntity.java
+++ src/main/java/data_access/persistence/UserEntity.java
@@ -1,12 +1,14 @@
 package data_access.persistence;
 
+import entity.User;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name= "users")
+@Table(name = "users")
 public class UserEntity {
 
     @Id
@@ -38,5 +40,16 @@ public class UserEntity {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public static UserEntity fromUser(User user) {
+        return new UserEntity(user.getUsername(), user.getPassword());
+    }
+
+    public static User toUser(UserEntity userEntity) {
+        if (userEntity == null)
+            return null;
+
+        return new User(userEntity.username, userEntity.password);
     }
 }
