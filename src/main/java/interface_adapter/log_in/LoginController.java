d36ca687c37baef2d10c352cdfaf9c1a779e0613
Anna <anna.tsvietkova@ukma.edu.ua>
2023-12-01 18:28:18 -0500

    Implemented interface adapter for Login use case.

---
+++ src/main/java/interface_adapter/log_in/LoginController.java
@@ -0,0 +1,19 @@
+package interface_adapter.log_in;
+
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginInputData;
+
+public class LoginController {
+    final LoginInputBoundary loginUseCaseInteractor;
+    public LoginController(LoginInputBoundary loginUseCaseInteractor) {
+        this.loginUseCaseInteractor = loginUseCaseInteractor;
+    }
+
+
+    public void execute(String username, String password) {
+        LoginInputData loginInputData = new LoginInputData(
+                username, password);
+
+        loginUseCaseInteractor.execute(loginInputData);
+    }
+}
