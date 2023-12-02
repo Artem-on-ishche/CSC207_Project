12298b1b41983c2d1452554743ac5f5e1917f39b
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2023-12-02 11:43:07 -0500

    basic implementation of FileImageCreator

--- src/main/java/data_access/FileImageCreator.java
+++ src/main/java/data_access/FileImageCreator.java
@@ -3,9 +3,19 @@ package data_access;
 import model.ImageData;
 import use_case.create_wardrobe.ImageCreator;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class FileImageCreator implements ImageCreator {
     @Override
     public ImageData fromImageSrc(String src) {
-        return null;
+        var file = new File(src);
+        try {
+            var fileContent = Files.readAllBytes(file.toPath());
+            return new ImageData(file, fileContent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
