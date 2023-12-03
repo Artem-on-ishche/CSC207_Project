be2dcc373acae2e42344c446f731864472a78b12
Artem Mykytyshyn <artem.mykytyshyn@gmail.com>
2023-12-02 19:34:51 -0500

    Create empty interactor test

---
+++ src/test/java/use_case/view_all_clothing_items/ViewAllClothingItemsInteractorTest.java
@@ -0,0 +1,33 @@
+package use_case.view_all_clothing_items;
+
+import model.ClothingItem;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ViewAllClothingItemsInteractorTest {
+
+//    private static final ImageCreator imageCreator = new FileImageCreator();
+//    private static final Image image = imageCreator.fromImageSrc("docs/img.png");
+//    private static final List<ClothingItem> clothingItems = List.of(
+//            new ClothingItem(null, itemName1, image, ClothingType.HEAD, 1, Optional.empty()),
+//            new ClothingItem(null, itemName2, image, ClothingType.LOWER_BODY, -5, Optional.empty())
+//    );
+//
+//    @BeforeEach
+//    void setUp() {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    @Test
+//    void execute() {
+//    }
+}
