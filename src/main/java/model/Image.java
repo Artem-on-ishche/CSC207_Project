da672d6b40fdc8b54ca0eb38ce056411faa1e88b
Dashap03 <plakhotnaia@gmail.com>
2023-12-06 06:06:47 +0200

    Added method for resizing image

--- src/main/java/model/Image.java
+++ src/main/java/model/Image.java
@@ -1,11 +1,19 @@
 package model;
 
+import data_access.persistence.FileImageCreator;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 public final class Image {
     private final File imageFile;
-    private final byte[] imageData;
+    private byte[] imageData;
 
     public Image(File imageFile, byte[] imageData) {
         this.imageFile = imageFile;
@@ -41,5 +49,22 @@ public final class Image {
                 "imageFile=" + imageFile +
                 ", imageData=" + Arrays.toString(imageData) +
                 '}';
+    }
+
+    public Image getScaledInstance(int desiredWidth, int desiredHeight, int scaleSmooth) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(imageData);
+        BufferedImage originalImage = ImageIO.read(bis);
+
+        var scaledImage = originalImage.getScaledInstance(desiredWidth, desiredHeight, scaleSmooth);
+
+        BufferedImage bufferedScaledImage = new BufferedImage(desiredWidth, desiredHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedScaledImage.createGraphics();
+        g2d.drawImage(scaledImage, 0, 0, null);
+        g2d.dispose();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedScaledImage, "jpg", baos);
+        byte[] scaledImageData = baos.toByteArray();
+
+        return new Image(imageFile, scaledImageData);
     }
 }
